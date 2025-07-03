8e3edf61bd55eac7988b42d8d0d2736293773cc5
Joshua <1114joshuafeliciano@gmail.com>
2025-07-03 10:44:03 +0800

    ReplacedResume

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Home from "./pages/Home";
-import Projects from "./pages/Project";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Layout from "./theme/Layout";
-import Experience from "./pages/Experience";
-import { inject } from '@vercel/analytics';
+function App() {
+  const [count, setCount] = useState(0)
 
-inject();
-
-
-
-export default function App() {
   return (
-    <div className="bg-brand text-white min-h-screen">
-
-      <Layout>
-        <Navbar />
-        <main className="p-4 max-w-5xl mx-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </Layout>
-    </div>
-
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
